9a69c27fe9fa0b72cd888e9ebc5be2452c1f7178
JungHyeonKim <dev.kjung@gmail.com>
2025-08-11 22:29:18 +0900

    [Feat]: create project

---
+++ src/main/java/com/kjung/springhwpprocessor/SpringHwpProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.kjung.springhwpprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHwpProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHwpProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjung/springhwpprocessor/SpringHwpProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjung.springhwpprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHwpProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
